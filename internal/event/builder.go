e2da38953d49c6e53b2bcdce1f7d409cf52be263
Anshuman Mandal <anshumanmandal@Anshumans-MacBook-Air.local>
2026-02-13 08:22:25 +0530

    chore: Set seq as uint64 instead of int64 in event builder

--- internal/event/builder.go
+++ internal/event/builder.go
@@ -4,17 +4,22 @@ import (
 	"fmt"
 )
 
-func Build(freq Frequency, ts int64, seq int64) Event {
+func Build(freq Frequency, ts int64, seq uint64) Event {
 	id := buildID(freq, ts, seq)
-
+	seed := buildSeed(ts, seq)
 	return Event{
 		ID:        id,
 		Frequency: freq,
 		Timestamp: ts,
 		Sequence:  seq,
+		Seed:      seed,
 	}
 }
 
-func buildID(freq Frequency, ts int64, seq int64) string {
+func buildID(freq Frequency, ts int64, seq uint64) string {
 	return fmt.Sprintf("%d-%d-%d", freq, ts, seq)
+}
+
+func buildSeed(ts int64, seq uint64) int64 {
+	return ts ^ int64(seq)
 }
