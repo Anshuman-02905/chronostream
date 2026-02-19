8e58b94e460e3ef421cd560aed0def73a1ed00f9
Anshuman Mandal <anshumanmandal@Anshumans-MacBook-Air.local>
2026-02-19 21:59:55 +0530

    wip: scaffold scheduler and time helpers - Add RealScheduler implementation and factory - Implement durationFor helper - Add partial nextboundary calculation - Add Start method stub

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -2,6 +2,7 @@ package scheduler
 
 import (
 	"context"
+	"time"
 
 	"github.com/Anshuman-02905/chronostream/internal/event"
 	"github.com/Anshuman-02905/chronostream/internal/monotime"
@@ -49,6 +50,34 @@ func New(freq event.Frequency, ts monotime.TimeSource, bufferSize int) *RealSche
 	}
 }
 
-func durationFor()  {}
-func Start()        {}
-func nextboundary() {}
+// This helps keep Star()clean
+//
+//	Avoids use of magic number
+//	makes future entention easier
+//	and Centralizes frequency behaviour
+func durationFor(freq event.Frequency) time.Duration {
+	switch freq {
+	case event.FrequencySecond:
+		return time.Second
+	case event.FrequencyMinute:
+		return time.Minute
+	case event.FrequencyHour:
+		return time.Hour
+	case event.FrequencyDay:
+		return 24 * time.Hour
+	default:
+		panic("unsupported frequency")
+	}
+}
+
+// principle instead of adding duration to Now truncate to boundary then add + 1 unit this will help us lock to the real wall clock
+func nextboundary(now time.Time, freq event.Frequency) time.Time {
+	switch freq {
+	case event.FrequencySecond:
+		truncated := now.Truncate(time.Second)
+		return truncated.Add(time.Second)
+
+	}
+}
+
+func Start() {}
